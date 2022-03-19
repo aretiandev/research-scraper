1e5d5e3392277f95912c49ad5bed2616ffe3a283
yufernando <yufernando@gmail.com>
2022-03-19 00:44:28 -0400

    feat(log_handler): add log_handler to send Slack msgs

---
+++ src/log_handler.py
@@ -0,0 +1,44 @@
+from slack_sdk import WebClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+slack_member_id = os.environ["SLACK_MEMBER_ID"]
+client = WebClient(token=slack_token)
+
+
+def log_handler(msg):
+    msg_level = msg.get('level')
+    if not msg_level:
+        return
+
+    if msg_level == 'run_info':
+        message = ':snake: ' + msg['msg']
+    elif msg_level == 'job_info':
+        message_lst = [
+            f":runner: Running rule '{msg['name']}':",
+            f"    jobid: {msg['jobid']}",
+            f"    input: {msg['input']}" if msg['input'] else "",
+            f"    output: {msg['output']}" if msg['output'] else "",
+            f"    wildcards: {msg['wildcards']}" if msg['wildcards'] else ""
+            ]
+        message_lst = [x for x in message_lst if x]
+        message = "\n".join(message_lst)
+    elif msg_level == 'job_finished':
+        message = f":white_check_mark: Finished job {msg['jobid']}."
+    elif msg_level == 'progress':
+        message = f":construction: {msg['done']} of {msg['total']} steps ({msg['done']/msg['total']*100:.0f}%) done"
+    elif msg_level == 'job_error':
+        message = f"Error in rule '{msg['name']}': jobid {msg['jobid']}"
+        print(msg)
+    elif msg_level == 'error':
+        message = msg['msg']
+        print(msg)
+    else:
+        return
+
+    client.chat_postMessage(
+        channel=slack_member_id,
+        text=message
+        )
