9f0ad9d5558f97cc11f53957204386df4772a649
yufernando <yufernando@gmail.com>
2022-06-06 23:18:30 +0000

    feat: add prune rule

--- scripts/prune.py
+++ scripts/prune.py
@@ -2,11 +2,18 @@ import sqlite3
 import subprocess
 import sys
 
+from src.logging import configLogger
+
+log = configLogger(snakemake.rule, snakemake.config["logfile"])  # type: ignore # noqa
+
 
 def main(db):
     subprocess.run(["rm", "-rf", "output"])
+    log.info("Deleted 'output/'.")
     subprocess.run(["rm", "-rf", "log"])
+    log.info("Deleted 'log/'.")
     subprocess.run(["rm", "saopaulo.db"])
+    log.info("Deleted 'saopaulo.db'.")
 
     conn = sqlite3.connect(db)
     c = conn.cursor()
@@ -24,6 +31,7 @@ def main(db):
 
     conn.commit()
     conn.close()
+    log.info("Created 'saopaulo.db' and table 'urls'.")
 
 
 if __name__ == "__main__":
