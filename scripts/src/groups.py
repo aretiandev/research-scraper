ad0193312c080403c7979022b6e1f01ffe237d85
Caleb Yee <calebyee@Calebs-MacBook-Pro.local>
2024-08-15 15:03:31 -0400

    Cleaning Group-level data, cont.

--- scripts/src/groups.py
+++ scripts/src/groups.py
@@ -14,6 +14,8 @@ def create_group_networks(
     input_nodes, input_groups, input_edges, output_group_nodes, output_group_edges
 ):
 
+    # Get Nodes
+    log.info(f"Loading: {input_nodes}")
     author_df = pd.read_csv(input_nodes, converters={"groups": eval})
 
     # Filter nodelist for researchers with nonempty research groups
@@ -24,34 +26,49 @@ def create_group_networks(
     author_gp_lst = list(author_gp_df["id"].unique())
 
     author_gp_df = author_gp_df.copy()
-    author_gp_df.loc[:,"url_id"] = author_gp_df["groups"].apply(lambda x: x[0][1:])
+    
+    full_author_gp_df = author_gp_df.explode('groups').reset_index(drop=True)
+
+    full_author_gp_df["url_id"] = full_author_gp_df["groups"].str.slice(1)
 
     # Get group names
     group_df = pd.read_csv(input_groups)
     group_df["url_id"] = group_df["url"].str[31:]
+    
+    def safe_eval(x):
+        if isinstance(x, str):  # Only evaluate if x is a string
+            return eval(x)
+        return []
 
     if 'researcher_names' in group_df.columns:
         group_df = group_df[["name", "principal_names", "researcher_names", "url_id"]]    
-        group_df['principal_names'] = group_df['principal_names'].apply(lambda x: x if isinstance(x, list) else ([] if pd.isna(x) else [x]))
-        group_df['researcher_names'] = group_df['researcher_names'].apply(lambda x: x if isinstance(x, list) else ([] if pd.isna(x) else [x]))
+        group_df['principal_names'] = group_df['principal_names'].apply(safe_eval)
+        group_df['researcher_names'] = group_df['researcher_names'].apply(safe_eval)
 
         # Combine the lists from 'principal_names' and 'researcher_names' into 'total_names'
         group_df['total_names'] = group_df.apply(lambda row: row['principal_names'] + row['researcher_names'], axis=1)
+
     else:
         group_df = group_df[["name", "principal_names", "url_id"]]    
-        group_df =group_df.rename(columns={'principal_names':'total_names'})
+        group_df = group_df.rename(columns={'principal_names':'total_names'})
+        group_df['total_names'] = group_df['total_names'].apply(safe_eval)
+    
+    group_df['n_researchers'] = group_df['total_names'].apply(len)
 
-    author_gp_df = author_gp_df.merge(group_df, how="left", on="url_id")
-    author_gp_df = author_gp_df[['id', 'label', 'institution_2', 'department', 'institution', 'institution_group',
-            'n_publications', 'n_articles', 'n_chapters', 'n_books', 'n_other',
-            'url_id', 'name', 'total_names']]
+    full_author_gp_df = full_author_gp_df.merge(group_df, how="left", on="url_id")
+    full_author_gp_df = full_author_gp_df[['url_id', 'name', 'institution', 'institution_2','institution_group', 'department', 'total_names', 'n_researchers',
+            'n_publications', 'n_articles', 'n_chapters', 'n_books', 'n_other']]
 
-    qual_nodes_df = author_gp_df.groupby("url_id")[["name","institution","institution_2", "institution_group", "department","total_names"]].first().reset_index()
-    quant_nodes_df = author_gp_df.groupby("url_id").sum()
-
-    nodes_df = qual_nodes_df.merge(quant_nodes_df, how='left', on='url_id')
+    columns_to_sum = ['n_publications', 'n_articles', 'n_chapters', 'n_books', 'n_other']
+    
+    
+    agg_dict = {col: 'first' for col in full_author_gp_df.columns if col not in columns_to_sum}
+    agg_dict.update({col: 'sum' for col in columns_to_sum})
+    
+    nodes_df = full_author_gp_df.groupby('url_id').agg(agg_dict)
     nodes_df = nodes_df.rename(columns={"url_id": "id", "name": "label"})
-
+    nodes_df = nodes_df.dropna(subset=['label'])
+    
     # Save
     nodes_df.to_csv(output_group_nodes, index=None)
     log.info(f"Saved: {output_group_nodes}")
@@ -66,11 +83,12 @@ def create_group_networks(
     )
     edges_gp_df = edges_df[mask]
     edges_gp_df = edges_gp_df.merge(
-        author_gp_df[["id", "url_id"]], how="left", left_on="Source", right_on="id"
+        full_author_gp_df[["id", "url_id"]], how="left", left_on="Source", right_on="id"
     )
+    #you've effectively changed author_gp_df to full_author_gp_df line 50
     edges_gp_df = edges_gp_df.rename(columns={"url_id": "Source_gp"})
     edges_gp_df = edges_gp_df.merge(
-        author_gp_df[["id", "url_id"]], how="left", left_on="Target", right_on="id"
+        full_author_gp_df[["id", "url_id"]], how="left", left_on="Target", right_on="id"
     )
     edges_gp_df = edges_gp_df.rename(columns={"url_id": "Target_gp"})
     edges_gp_df = edges_gp_df[["Source_gp", "Target_gp", "Weight"]]
